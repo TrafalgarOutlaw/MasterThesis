5adecd3d9b7c6e68c726fcea114785f51255e187
TrafalgarOutlaw <eherwin95@googlemail.com>
2022-05-12 03:35:20 +0200

    FEATURE: added missing commit files

---
+++ VR-TRPG/Assets/Scripts/Movement/PlayerCharacter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRTRPG.Movement
+{
+    public class PlayerCharacter : MonoBehaviour
+    {
+        public int walkDistance = 4;
+    }
+}
